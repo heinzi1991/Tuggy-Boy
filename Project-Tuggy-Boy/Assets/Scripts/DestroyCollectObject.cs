359ab9853be0c469c1a0faafbbef3828dcb4b5f9
Martin Zagar <martin.zagar@student.tugraz.at>
2016-05-04 17:45:21 +0200

    test destroy objects

---
+++ Project-Tuggy-Boy/Assets/Scripts/DestroyCollectObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyCollectObject : MonoBehaviour {
+
+	void OnCollisionEnter (Collision col) {
+
+		if(col.gameObject.name == "CollectObject") {
+
+			Debug.Log ("HELLO");
+			Destroy (col.gameObject);
+		}
+	}
+}
---
+++ Project-Tuggy-Boy/Assets/Scripts/OnCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnCollision : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col) {
+
+		Debug.Log ("Enter called");
+	}
+
+	void OnCollisionStay(Collision col) {
+
+		Debug.Log ("Stay occuring..");
+	}
+
+	void OnCollisionExit(Collision col) {
+
+		Debug.Log ("Exit called");
+	}
+}
