bb3eb8539dd605518a6a4685a0fe4ee479705b3e
Martin Zagar <theburtonwear@gmx.at>
2016-05-13 00:01:14 +0200

    final push! (prototyp)

--- Project-Tuggy-Boy/Assets/Scripts/DestroyCollectObject.cs
+++ Project-Tuggy-Boy/Assets/Scripts/DestroyCollectObject.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class DestroyCollectObject : MonoBehaviour {
 
+	private static int destroy = 0;
+
 	void OnCollisionEnter (Collision col) {
 
 		if(col.gameObject.name == "CollectObject") {
 
 			Destroy (col.gameObject);
+			destroy++;
 		}
+	}
+
+	public static int getDestroy() {
+
+		return destroy;
 	}
 }
---
+++ Project-Tuggy-Boy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+
+	public GameObject gui;
+	public EnemyManager enemyManager;
+	public int collectCount;
+	public GameObject player;
+
+	private Text guiText;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		guiText = gui.GetComponentInChildren<Text>();
+		guiText.text = "-- / " + collectCount;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		guiText.text = DestroyCollectObject.getDestroy () + " / " + collectCount;
+
+		if (DestroyCollectObject.getDestroy() == collectCount) {
+
+			SceneManager.LoadScene ("Start Menu");
+		}
+	
+	}
+}
