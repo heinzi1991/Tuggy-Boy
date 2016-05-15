86116a7b0c746c325919781e0af09c557720b527
Martin Zagar <theburtonwear@gmx.at>
2016-05-16 00:18:57 +0200

    change little things
    add a little enemy ai
    and change the particle system of haris

--- Project-Tuggy-Boy/Assets/Scripts/DestroyCollectObject.cs
+++ Project-Tuggy-Boy/Assets/Scripts/DestroyCollectObject.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class DestroyCollectObject : MonoBehaviour {
 
+	public GameObject particle;
+
+
 	private static int destroy = 0;
 
 	void OnCollisionEnter (Collision col) {
 
 		if(col.gameObject.name == "CollectObject") {
 
+			Instantiate(particle, new Vector3(transform.position.x, 1, transform.position.z + 1), particle.transform.rotation);
 			Destroy (col.gameObject);
 			destroy++;
 		}
---
+++ Project-Tuggy-Boy/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	public Transform target;
+
+	private NavMeshAgent agent;
+
+	void Start() {
+
+		agent = gameObject.GetComponent<NavMeshAgent> ();
+		agent.SetDestination(target.position);
+	}
+}
