764fdec72462db92652f28e951b6e2a6abb59e9d
Martin Zagar <theburtonwear@gmx.at>
2016-06-21 23:07:07 +0200

    add waypoints (footprints)

---
+++ Project-Tuggy-Boy/Assets/Scripts/CreateWayPoint.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateWayPoint : MonoBehaviour {
+
+	private bool cubeExist = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit(Collider other) {
+
+		if(other.name == "FPSController") {
+
+			if (cubeExist == false) {
+
+				GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				cube.transform.position = new Vector3(this.transform.position.x, 0.16f, this.transform.position.z);
+				cube.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+				cubeExist = true;
+			}
+		}
+	}
+}
